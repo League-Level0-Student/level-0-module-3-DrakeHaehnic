61fd601f9a741546157b2733f45543af3d156d08
Blast <Blast@Uber-Puter.lan>
2020-09-28 18:27:32 -0700

    rtgbrdthbtdubjyj

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -2,12 +2,50 @@
 package _01_else_if._1_robot_color_chooser;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		
 		//1. Create a new Robot
+	
+		Robot a = new Robot();
+		a.changeRobot("https://i.etsystatic.com/20770271/c/2123/1688/127/372/il/a0d8ae/2238302975/il_340x270.2238302975_eacy.jpg");
+		a.setSpeed(3000);
+		a.setPenWidth(10);
+		a.penDown();
+		for (int i = 0; i < 99999999; i++) {
+				for (int c = 0; c <2000000000; c++) {
+			a.move(c);
+			a.turn(200);
+			a.move(c);
+			a.turn(200);
+		//	String b = JOptionPane.showInputDialog("name your favorite color");
+			//if (b .equals ("yellow")) {
+				a.setPenColor(Color.MAGENTA);
+		//	}
+		//	else if (b .equals ("magenta")) {
+				a.setPenColor(Color.yellow);
+		//	}
+			
+		//	else if (b .equals ("black")) {
+				a.setPenColor(Color.PINK);
+			//}
+		//	else if (b .equals ("pink")) {
+				a.setPenColor(Color.black);
+		//	}
+		//	else{
+				a.setRandomPenColor();
+			//}
+			a.move(c);
+			a.turn(200);
+			a.move(c);
+			a.turn(200);
+		}
+		
 	    //2. Make the robot draw a shape (this will take more than one line of code)		
 		//3. Set the pen width to 10
 		//4. Ask the user what color pen they would like the robot to draw with
@@ -21,4 +59,5 @@ public class RobotColorChooser {
 
 
 	}
+}
 }
